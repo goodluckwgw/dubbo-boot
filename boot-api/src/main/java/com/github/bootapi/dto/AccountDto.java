2432b10b7b4f1e8b46d976f38b38c480fece0676
willie <670688199@qq.com>
2015-12-28 16:05:56 +0800

    dubbo泛化测试

--- boot-api/src/main/java/com/github/bootapi/dto/AccountDto.java
+++ boot-api/src/main/java/com/github/bootapi/dto/AccountDto.java
@@ -13,6 +13,44 @@ public class AccountDto implements Serializable{
 	public Long id;
 	public String email;
 	public String name;
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
 
 	@Override
 	public String toString() {
